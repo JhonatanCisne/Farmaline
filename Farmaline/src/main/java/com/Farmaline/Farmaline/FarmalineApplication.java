b99a3b03ce566e4cd31a120a4a4de27efcb711b6
Cisneros Jorge Jhonatan Eduardo <Jhonatanjorge151@gmail.com>
2025-05-31 23:56:05 -0500

    carpeta spring

---
+++ Farmaline/src/main/java/com/Farmaline/Farmaline/FarmalineApplication.java
@@ -0,0 +1,13 @@
+package com.Farmaline.Farmaline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmalineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmalineApplication.class, args);
+	}
+
+}
---
+++ Farmaline/src/test/java/com/Farmaline/Farmaline/FarmalineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Farmaline.Farmaline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmalineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
